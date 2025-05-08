97a78569827caf81158818602845ef5afd620ce0
mrBidone <ab.bz@ukr.net>
2025-05-08 19:43:03 +0200

    Add modal-review and modal-subscr with completed mob-styles

---
+++ src/js/modal-review.js
@@ -0,0 +1,33 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelectorAll('[data-modal-review-open]'),
+    closeModalBtn: document.querySelector('[data-modal-review-close]'),
+    modal: document.querySelector('[data-modal-review]'),
+  };
+
+  refs.openModalBtn.forEach(btn => {
+    btn.addEventListener('click', toggleModal);
+  });
+
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  refs.modal.addEventListener('click', event => {
+    if (event.target === refs.modal) {
+      toggleModal();
+    }
+  });
+
+  document.addEventListener('keydown', event => {
+    if (
+      event.key === 'Escape' &&
+      refs.modal.classList.contains('modal-is-open')
+    ) {
+      toggleModal();
+    }
+  });
+
+  function toggleModal() {
+    refs.modal.classList.toggle('modal-is-open');
+    document.body.classList.toggle('no-scroll');
+  }
+})();
--- src/main.js
+++ src/main.js
@@ -2,3 +2,5 @@ import './js/products.js';
 import './js/reviews.js';
 import './js/header.js';
 import './js/modal-buy-now.js';
+import './js/modal-review.js';
+import './js/modal-subscribe.js';
